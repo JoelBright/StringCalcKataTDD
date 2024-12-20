cad8e0c94fbb88f2996f3f40e15681f70df9c43f
JoelBright <joelbright@live.com>
2024-12-20 13:21:08 +0530

    Generated initial boilerplate code

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  public static void main(String[] args) {
+    String numbers = "";
+    int answer = add(numbers);
+    System.out.println(answer);
+  }
+
+  private static int add(String numbers) {
+    if(numbers.isEmpty())
+      return 0;
+    else {
+      return 1;
+    }
+  }
+}
